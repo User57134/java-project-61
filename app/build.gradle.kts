88cf714dbe1ab80a18d6b0e08ffbc10cad7a35bb
Player One <pl17dev@gmail.com>
2026-02-16 23:29:22 +0300

    SonarQube is set up and added to the project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,16 @@
 plugins {
     id("com.github.ben-manes.versions") version "0.53.0"
+    id("org.sonarqube") version "7.1.0.6387"
     application
 }
+
+sonar {
+    properties {
+        property("sonar.projectKey", "User57134_java-project-61")
+        property("sonar.organization", "user57134")
+    }
+}
+
 
 java {
     toolchain {
@@ -9,16 +18,20 @@ java {
     }
 }
 
+
 tasks.getByName("run", JavaExec::class) {
     standardInput = System.`in`
 }
 
+
 group = "hexlet.code"
 version = "1.0-SNAPSHOT"
+
 
 repositories {
     mavenCentral()
 }
+
 
 application {
     mainClass.set("hexlet.code.App")
