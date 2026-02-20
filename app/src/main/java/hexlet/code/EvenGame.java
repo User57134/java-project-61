60b23b0290e73c9ef88016c900a4d70669b95aa5
Player One <pl17dev@gmail.com>
2026-02-20 22:57:58 +0300

    fixing sonnar error: SecureRandom instead Random

--- app/src/main/java/hexlet/code/EvenGame.java
+++ app/src/main/java/hexlet/code/EvenGame.java
@@ -1,12 +1,12 @@
 package hexlet.code;
 
 import java.util.Scanner;
-import java.util.Random;
+import java.security.SecureRandom;
 
 public final class EvenGame {
     public static final int DEFAULT_RANDOM_MAX = 100;
     public static final int DEFAULT_TRIES_NUMBER = 3;
-    private static Random generator = null;
+    private static SecureRandom generator = null;
 
     private EvenGame() {
 
@@ -27,7 +27,7 @@ public final class EvenGame {
         System.out.println("Answer 'yes' if the number is even, otherwise answer 'no'.");
 
         if (generator == null) {
-            generator = new Random();
+            generator = new SecureRandom();
         }
 
         for (int i = 1; i <= DEFAULT_TRIES_NUMBER; i += 1) {
